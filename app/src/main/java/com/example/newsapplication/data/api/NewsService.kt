db998e01df9773710eb70610239479038ba5fa29
Samandar <karimzonovsamandar@gmail.com>
2023-01-30 12:34:47 +0500

    News

---
+++ app/src/main/java/com/example/newsapplication/data/api/NewsService.kt
@@ -0,0 +1,22 @@
+package com.example.newsapplication.data.api
+
+import com.example.newsapplication.utils.Constants.Companion.API_KEY
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface NewsService {
+
+    @GET("/v2/everything")
+    suspend fun getEverything(
+        @Query("q") query: String,
+        @Query("page") page: Int = 1,
+        @Query("apiKey") apiKey: String = API_KEY
+
+    )
+
+    suspend fun getHeadlines(
+        @Query("country") countryCode: String = "ru",
+        @Query("page") page: Int = 1,
+        @Query("apiKey") apiKey: String = API_KEY
+    )
+}
---
+++ app/src/main/java/com/example/newsapplication/di/AppModule.kt
@@ -0,0 +1,43 @@
+package com.example.newsapplication.di
+
+import com.example.newsapplication.data.api.NewsService
+import com.example.newsapplication.utils.Constants.Companion.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class AppModule {
+
+    @Provides
+    fun baseUrl() = BASE_URL
+
+    @Provides
+    fun logging() = HttpLoggingInterceptor()
+        .setLevel(HttpLoggingInterceptor.Level.BODY)
+
+    @Provides
+    fun okHttpClient() = OkHttpClient.Builder()
+        .addInterceptor(logging())
+        .build()
+
+    @Provides
+    @Singleton
+    fun providesRetrofit(baseUrl: String):NewsService =
+
+        Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient())
+            .build()
+            .create(NewsService::class.java)
+
+
+}
